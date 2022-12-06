6ef6108fa3d7e1756749b5ed071a49d82cfe1cc0
shk03 <73791353+shk03@users.noreply.github.com>
2022-12-06 11:26:45 +0100

    added node version to package.json

--- app.js
+++ app.js
@@ -122,6 +122,9 @@ app.post("/delete", function(req, res) {
   }
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
 });
+
